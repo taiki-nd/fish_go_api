c0e411903d0a1aa33c94cc3f90e13e5c2df87c9d
taiki-nd <taiki.nd.real@gmail.com>
2022-05-31 21:31:51 +0900

    fiberの導入

--- main.go
+++ main.go
@@ -4,10 +4,20 @@ import (
 	"fish_go_api/config"
 	"fish_go_api/utils"
 
+	"github.com/gofiber/fiber/v2"
+
 	"log"
 )
 
 func main() {
 	utils.Logging(config.Config.Logfile)
-	log.Println("hello world!")
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World 👋!")
+	})
+
+	log.Println("starting server at port:8000")
+
+	app.Listen(":8000")
 }
