ede8d1fa20429dcb37090ebd91d3ac80c19891be
taiki-nd <taiki.nd.real@gmail.com>
2022-05-31 20:51:17 +0900

    configの作成

---
+++ config/config.go
@@ -0,0 +1,24 @@
+package config
+
+import (
+	"log"
+	"os"
+
+	"gopkg.in/ini.v1"
+)
+
+type ConfigList struct {
+	Logfile string
+}
+
+var Config ConfigList
+
+func init() {
+	cfg, err := ini.Load("config/config.ini")
+	if err != nil {
+		log.Printf("failed to load config.ini: %v", err)
+		os.Exit(1)
+	}
+
+	Config = ConfigList{}
+}
