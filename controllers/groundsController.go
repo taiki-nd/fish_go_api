a835e0668c976caff5ae41a4575d516da9aed6fe
taiki-nd <taiki.nd.real@gmail.com>
2022-06-04 13:12:40 +0900

    アソシエーション用のモデルの作成

--- controllers/groundsController.go
+++ controllers/groundsController.go
@@ -10,20 +10,6 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
-type GroundStyle struct {
-	Name    string `json:"name" gorm:"not null; size:256"`
-	Address string `json:"address" gorm:"not null; size:256"`
-	Tell    string `json:"tell" gorm:"not null; size:256"`
-	Email   string `json:"email" gorm:"not null; size:256"`
-	Break   string `json:"break" gorm:"not null; size:256"`
-	Styles  []int  `json:"styles" gorm:"many2many:ground_styles"`
-	Price   string `json:"price" gorm:"not null; size:256"`
-	Url     string `json:"url" gorm:"not null; size:256"`
-	Feature string `json:"feature" gorm:"not null; size:256"`
-	Rule    string `json:"rule" gorm:"not null; size:256"`
-	Other   string `json:"other" gorm:"not null; size:256"`
-}
-
 /*
 	Index ground
 */
@@ -65,7 +51,7 @@ func GroundsCreate(c *fiber.Ctx) error {
 
 	log.Println("start to create ground")
 
-	var groundStyle GroundStyle
+	var groundStyle models.GroundAssociation
 
 	err := c.BodyParser(&groundStyle)
 	if err != nil {
