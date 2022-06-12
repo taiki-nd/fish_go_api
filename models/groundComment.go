bf708620e1c6f74d102825ad89a0cf66e64d8a1a
taiki-nd <taiki.nd.real@gmail.com>
2022-06-12 22:20:52 +0900

    ground_commentモデルの作成

---
+++ models/groundComment.go
@@ -0,0 +1,16 @@
+package models
+
+import (
+	"time"
+)
+
+type GroundComment struct {
+	Id        uint      `json:"id" gorm:"primarykey"`
+	Name      string    `json:"name" gorm:"not null; size:256"`
+	Content   string    `json:"content" gorm:"not null; size:256"`
+	Rate      int64     `json:"Rate"`
+	Size      int64     `json:"Size"`
+	Nice      int64     `json:"Nice"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
