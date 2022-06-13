b41307ba240cfcd748f1d72c5ebe2f33c9639f9a
taiki-nd <taiki.nd.real@gmail.com>
2022-06-13 21:42:48 +0900

    アソシエーションの設定

--- models/ground.go
+++ models/ground.go
@@ -5,20 +5,22 @@ import (
 )
 
 type Ground struct {
-	Id        uint      `json:"id" gorm:"primarykey"`
-	Name      string    `json:"name" gorm:"not null; size:256"`
-	Address   string    `json:"address" gorm:"not null; size:256"`
-	Tell      string    `json:"tell" gorm:"not null; size:256"`
-	Email     string    `json:"email" gorm:"not null; size:256"`
-	Break     string    `json:"break" gorm:"not null; size:256"`
-	Styles    []Style   `json:"styles" gorm:"many2many:ground_styles"`
-	Howtos    []Howto   `json:"howtos" gorm:"many2many:ground_howtos"`
-	Fishes    []Fish    `json:"fishes" gorm:"many2many:ground_fishes"`
-	Price     string    `json:"price" gorm:"not null; size:256"`
-	Url       string    `json:"url" gorm:"not null; size:256"`
-	Feature   string    `json:"feature" gorm:"not null; size:256"`
-	Rule      string    `json:"rule" gorm:"not null; size:256"`
-	Other     string    `json:"other" gorm:"not null; size:256"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
+	Id              uint          `json:"id" gorm:"primarykey"`
+	Name            string        `json:"name" gorm:"not null; size:256"`
+	Address         string        `json:"address" gorm:"not null; size:256"`
+	Tell            string        `json:"tell" gorm:"not null; size:256"`
+	Email           string        `json:"email" gorm:"not null; size:256"`
+	Break           string        `json:"break" gorm:"not null; size:256"`
+	Styles          []Style       `json:"styles" gorm:"many2many:ground_styles"`
+	Howtos          []Howto       `json:"howtos" gorm:"many2many:ground_howtos"`
+	Fishes          []Fish        `json:"fishes" gorm:"many2many:ground_fishes"`
+	Price           string        `json:"price" gorm:"not null; size:256"`
+	Url             string        `json:"url" gorm:"not null; size:256"`
+	Feature         string        `json:"feature" gorm:"not null; size:256"`
+	Rule            string        `json:"rule" gorm:"not null; size:256"`
+	Other           string        `json:"other" gorm:"not null; size:256"`
+	GroundCommentId uint          `json:"ground_comment_id"`
+	GroundComment   GroundComment `json:"ground_comment" gorm:"foreignKey:GroundCommentId"`
+	CreatedAt       time.Time     `json:"created_at"`
+	UpdatedAt       time.Time     `json:"updated_at"`
 }
