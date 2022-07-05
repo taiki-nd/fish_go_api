5cdd71c41e1e9e0a1d2b385dd9877badd225a5e3
taiki-nd <kii.develop.0517@gmail.com>
2022-07-05 22:04:57 +0900

    デフォルト値の設定

--- models/commentReply.go
+++ models/commentReply.go
@@ -6,7 +6,7 @@ import (
 
 type CommentReply struct {
 	Id              uint      `json:"id" gorm:"primarykey"`
-	Name            string    `json:"name" gorm:"not null; size:256"`
+	Name            string    `json:"name" gorm:"not null; size:256; default:gest"`
 	Content         string    `json:"content" gorm:"not null; size:256"`
 	ImageUrl        string    `json:"image_url" gorm:"size:256"`
 	Filename        string    `json:"filename" gorm:"size:256"`
